7c84132d7d8d6a493800a78f3cdecf5659866eab
Mukhammed123 <muhammedmusaaa@gmail.com>
2022-06-24 17:10:01 +0800

    base url

--- src/services/apiPaths.js
+++ src/services/apiPaths.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://127.0.0.1:8000/api";
+export const baseUrl = "https://fast-cliffs-03764.herokuapp.com";
 export const todoPath = `${baseUrl}/todo/`;
 export const todoItemPath = `${baseUrl}/todo/list/`;
 export const loginPath = `${baseUrl}/user/login/`;

b6883b266a817666eaf274d187ff361107188b69
Mukhammed123 <muhammedmusaaa@gmail.com>
2022-07-03 22:55:26 +0800

    new users can now be registered

--- src/services/apiPaths.js
+++ src/services/apiPaths.js
@@ -1,5 +1,5 @@
-export const baseUrl = "http://127.0.0.1:8000/api";
-// export const baseUrl = "https://fast-cliffs-03764.herokuapp.com/api";
+// export const baseUrl = "http://127.0.0.1:8000/api";
+export const baseUrl = "https://fast-cliffs-03764.herokuapp.com/api";
 export const todoPath = `${baseUrl}/todo/`;
 export const todoItemPath = `${baseUrl}/todo/list/`;
 export const loginPath = `${baseUrl}/user/login/`;
